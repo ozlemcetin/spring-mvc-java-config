0999dbab4ab24d2e9d2f42fe8fe5196c8f94e2b1
ozlemcetin <ozlem.cetin@gmail.com>
2020-11-02 22:39:12 +0300

    Initial Commit- Introduction to Spring MVC

--- spring-ioc-xml-demo/src/main/java/com/timbuchalka/springdemo/domain/Department.java
+++
@@ -1,7 +0,0 @@
-package com.timbuchalka.springdemo.domain;
-
-public interface Department {
-
-	public String hiring(int numberOfRecruitments);
-
-}
--- spring-ioc-xml-demo/src/main/java/com/timbuchalka/springdemo/service/RecruitmentService.java
+++
@@ -1,7 +0,0 @@
-package com.timbuchalka.springdemo.service;
-
-public interface RecruitmentService {
-
-	public String recruitEmployess(String compnayName, String departmentName, int numberOfRecruitments);
-
-}
